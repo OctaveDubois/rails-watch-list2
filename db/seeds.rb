f6e4d00e10c08342168d2f0e966c164640784aae
Octave Dubois <octave.dubois@hotmail.fr>
2023-12-15 15:55:04 +0100

    Seed avec l'API ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,24 @@
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-List.create(name: "Mes meilleurs films d'action")
-Bookmark.create(comment: "Bon film", movie_id: Movie.last.id, list_id: List.last.id)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+require "json"
+require "open-uri"
+
+
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+puts "Destroying movie"
+
+puts "Creating movies from API"
+
+
+url = "http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key"
+movie_serialized = URI.open(url).read
+movies = JSON.parse(movie_serialized)["results"]
+movies.each do |movie|
+  Movie.create!(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}", rating: movie["vote_average"])
+end
+
+list1 = List.create(name: "Top 100 des meilleurs films d'action")
+list2 = List.create(name: "Les films à (re)voir <3")
+
+puts "Job is done !"
